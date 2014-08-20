045d1681774ec1af92bea381bc98c98ebf9f8805
Victor Ng <vicngtor@gmail.com>
2014-08-19 18:13:56 -0700

    Still can't connect. Verbose enabled but no idea why

--- Test/TcpCall/TcpCall.ino
+++ Test/TcpCall/TcpCall.ino
@@ -9,24 +9,27 @@
 
 
 /*
- * Getting this to work:
- * -Make sure you have Arduino IDE 1.5
- * -Go to your arduino IDE and point the sketchbook location to THIS PROJECT
- *  ROOT where libraries and hardware resides
- * -Restart your arduino IDE
- */
-
-/*
  * According to the new Arduino include specification, "When the user imports a
  * library into their sketch (from the "Tools > Import Library" menu), an
  * #include statement will be added for all header (.h) files in the src/
  * directory (but not its sub-folders)
- *
- * To get this to work:
- * -Run link.bash which will copy, not symlink, Oyoroi.h into the directory
- *  OyoroiPinoccio/libraries/pinoccio/src folder
  */
 #include <Oyoroi.h>
+
+/*
+ * Setup
+ * -Make sure you have Arduino IDE 1.5
+ * -Go to your arduino IDE and point the sketchbook location to THIS PROJECT
+ *  ROOT where libraries and hardware resides
+ * -Restart your arduino IDE
+ *
+ * Running:
+ * -Run link.bash which will copy, not symlink, Oyoroi.h into the directory
+ *  OyoroiPinoccio/libraries/pinoccio/src folder
+ * -Run `python -m SimpleHTTPServer` in another terminal session as a web server
+ */
+#define SERVER_LOCATION "10.106.57.36"
+
 
 #define SKETCH_NAME "Custom"
 #define SKETCH_BUILD -1
@@ -42,6 +45,8 @@ void loop() {
   Scout.loop();
   // Add custom loop code here
   connectToServer();
+
+  delay(3000);    // This is probably bad
 }
 
 
@@ -52,33 +57,26 @@ void connectToServer() {
   GSModule *gsPtr;
   gsPtr = &pinoccio::WifiModule::instance.bp()->gs;
 
-  Serial.println(1);
-
   GSTcpClient client = GSTcpClient(*gsPtr);
   IPAddress ip;
-  char* url = "http://192.168.19.114";
-
-  Serial.println(2);
+  char* url = SERVER_LOCATION;
 
   if (!gsPtr->parseIpAddress(&ip, url)) {
     ip = gsPtr->dnsLookup(url);
 
     if (ip == INADDR_NONE) {
       Serial.print(F("Failed to resolve "));
+      Serial.print(url);
       Serial.println(F(", reassociating to retry"));
       return;
     }
   }
-
-  Serial.println(3);
 
   if (!client.connect(ip, 8000)) {
     Serial.println(F("HQ connection failed, reassociating to retry"));
     pinoccio::WifiModule::instance.bp()->associate();
     return;
   }
-
-  Serial.println(4);
 
   client.print("Hello");   // Should print a raw GET or POST request
   client.flush();
